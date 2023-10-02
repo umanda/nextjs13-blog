d936ca07a3cf612f6cdf7fb76d750d1da624053d
Umanda Jayobandara <umanda.uj@gmail.com>
2023-10-02 16:47:52 +0530

    dynamic post by slug

---
+++ app/[category]/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { DUMMY_CATEGORIES, DUMMY_POSTS } from '@/DUMMY_DATA';
+import PaddingContainer from '@/components/layout/padding-container';
+import PostList from '@/components/post/post-lists';
+
+export const generateStactParams =async () => {
+    
+    return DUMMY_CATEGORIES.map((category) => {
+        return{ 
+            category : category.slug
+        }
+    });
+
+}
+
+const Page = ({ params }: {
+    params: {
+        category: string
+    }
+}) => {
+
+    const posts = DUMMY_POSTS.filter(
+        (post) => post.category.title.toLocaleLowerCase() === params.category);
+    return (
+        <PaddingContainer>
+            <PostList posts={posts}/>
+        </PaddingContainer>
+    )
+}
+
+export default Page
---
+++ app/post/[slug]/page.tsx
@@ -0,0 +1,25 @@
+import { DUMMY_POSTS } from '@/DUMMY_DATA'
+import React from 'react';
+
+export const generateStaticParams =async () => {
+    return DUMMY_POSTS.map((post) => {
+        return {
+            slug: post.slug 
+        }
+    });
+}
+
+const Page = ({params} : {params : {
+    slug : string
+}}) => {
+
+
+    const post = DUMMY_POSTS.find((post)=> post.slug === params.slug);
+
+
+  return (
+    <div>{post?.title}</div>
+  )
+}
+
+export default Page
